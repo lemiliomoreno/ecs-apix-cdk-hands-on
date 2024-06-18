f493a29abb4139067eb93b60e1ae662697971c99
Luis E Moreno <lemiliomoreno@gmail.com>
2024-06-17 19:44:10 -0600

    feat: add database deployment

--- ecs_apix_hands_on/ecs_apix_hands_on_stack.py
+++ ecs_apix_hands_on/ecs_apix_hands_on_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
+    aws_rds as rds,
 )
 from constructs import Construct
 
@@ -25,4 +26,22 @@ class EcsApixHandsOnStack(Stack):
         db_sg.add_ingress_rule(
             peer=ec2.Peer.any_ipv4(),
             connection=ec2.Port.tcp(5432)
+        )
+
+        database_cluster = rds.DatabaseCluster(
+            self,
+            "EcsApixDbCluster",
+            engine=rds.DatabaseClusterEngine.aurora_postgres(
+                version=rds.AuroraPostgresEngineVersion.VER_16_1,
+            ),
+            serverless_v2_max_capacity=16,
+            serverless_v2_min_capacity=0.5,
+            credentials=rds.Credentials.from_generated_secret("clusteradmin"),
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+            ),
+            writer=rds.ClusterInstance.serverless_v2("writer1"),
+            security_groups=[
+                db_sg,
+            ]
         )
